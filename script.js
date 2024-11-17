366b44a448116a64a8d566643906d5c5f0e25286
Sergio Romero <sromero147@gmail.com>
2024-11-17 10:27:44 +0100

    New items

--- script.js
+++ script.js
@@ -4,35 +4,35 @@ const sections = {
         name: "Tales of Zestiria",
         price: "2.83",
         priority: 3,
-        link: "https://example.com/game-console",
+        link: "https://www.instant-gaming.com/es/860-comprar-tales-of-zestiria-pc-juego-steam/",
         image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEju9EXrY8GwurVlxB91w-OVGViz7bFASgClixC_fHqkJGsIHvmJqc7-SiC3Bat_0zt2k405D0iL3LhNs9HO-UvNfd1ieXGXdUJMJBxipZr1rQf7yMHEzwswaOtQufZjeeNjkXEwseBf846Z/s640/Zestiria.jpg"
       },
       {
         name: "Dark Souls 3",
         price: "22.99",
         priority: 5,
-        link: "https://example.com/latest-game",
+        link: "https://www.instant-gaming.com/es/857-comprar-dark-souls-3-pc-juego-steam/",
         image: "https://static.bandainamcoent.eu/high/dark-souls/dark-souls-3/00-page-setup/ds3_game-thumbnail.jpg"
       },
       {
         name: "Bayoneta",
         price: "4.88",
         priority: 3,
-        link: "https://example.com/latest-game",
+        link: "https://www.instant-gaming.com/es/2029-comprar-bayonetta-pc-juego-steam/",
         image: "https://gaming-cdn.com/images/products/2029/616x353/bayonetta-pc-juego-steam-cover.jpg?v=1666601337"
       },
       {
         name: "Tales of Arise",
         price: "9.99",
         priority: 4,
-        link: "https://example.com/latest-game",
+        link: "https://www.instant-gaming.com/es/5870-comprar-tales-of-arise-pc-juego-steam/",
         image: "https://i.blogs.es/166345/tales-of-arise-01/1366_2000.jpeg"
       },
       {
         name: "The Last Of US",
         price: "34.64",
         priority: 4,
-        link: "https://example.com/latest-game",
+        link: "https://www.instant-gaming.com/es/13613-comprar-steam-the-last-of-us-part-i-digital-deluxe-edition-digital-deluxe-edition-pc-juego-steam-europe/",
         image: "https://cdn1.epicgames.com/offer/0c40923dd1174a768f732a3b013dcff2/EGS_UpgradetoDigitalDeluxeEdition_NaughtyDogLLC_AddOn_S1_2560x1440-d0195796f9b15e41ee69393bb95c4edc"
       }
     ],
@@ -47,10 +47,24 @@ const sections = {
         image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-13-pro-max-blue-2023?wid=2000&hei=1897&fmt=jpeg&qlt=95&.v=1679072989205"
       },
       {
+        name: "Bateria XBox",
+        price: "13,63",
+        priority: 5,
+        link: "https://es.aliexpress.com/item/1005007654979638.html",
+        image: "https://ae-pic-a1.aliexpress-media.com/kf/S9f40e6b0223b4adebe0599fe102a62afC/Bater-a-recargable-de-2x3600mWh-para-Xbox-Series-X-S-Xbox-One-S-X-bater-a.png_.webp"
+      },
+      {
+        name: "Auriculares",
+        price: "4,92",
+        priority: 5,
+        link: "https://es.aliexpress.com/item/1005006660776200.html",
+        image: "https://ae-pic-a1.aliexpress-media.com/kf/S7ba6846931c24bfdafc598c76b4f2f538/Auriculares-KZ-EDX-PRO-X-IEM-con-unidad-din-mica-auriculares-HiFi-con-sonido-de-graves.jpg_.webp"
+      },
+      {
         name: "Sony XM4",
         price: "229",
         priority: 4,
-        link: "https://example.com/jeans",
+        link: "https://www.amazon.es/Sony-WH1000XM4-Auriculares-inal%C3%A1mbricos-Cancelling/dp/B08C7KG5LP/ref=sr_1_1_sspa?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&nsdOptOutParam=true&sr=8-1-spons&ufe=INHOUSE_INSTALLMENTS%3AES_IHI_4M_VF&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
         image: "https://www.sony.es/image/5d02da5df552836db894cead8a68f5f3?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF"
       },
       {
@@ -64,24 +78,31 @@ const sections = {
         name: "Xbox Elite Series",
         price: "112",
         priority: 4,
-        link: "https://example.com/jeans",
+        link: "https://www.amazon.es/Microsoft-Mando-Elite-Wireless-Controller/dp/B07SVZXBMG",
         image: "https://thumb.pccomponentes.com/w-530-530/articles/1058/10586654/2614-microsoft-xbox-elite-series-2-core-mando-inalambrico-blanco-comprar.jpg"
       }
     ],
-    "Clothes": [
+    "Other": [
       {
-        name: "T-Shirt",
-        price: "25",
+        name: "Peluche",
+        price: "13.89",
         priority: 3,
-        link: "https://example.com/tshirt",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
+        link: "https://es.aliexpress.com/item/1005007053635874.html",
+        image: "https://ae-pic-a1.aliexpress-media.com/kf/S45fc637f8bdc468a800e203c74e36b7ex/Juguete-de-peluche-de-Hotel-Hazbined-para-ni-os-Kawaii-Husk-Meow-mu-eca-de-Cosplay.jpg_.webp"
       },
       {
-        name: "Jeans",
-        price: "50",
-        priority: 2,
-        link: "https://example.com/jeans",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
+        name: "Carro compra",
+        price: "26.99",
+        priority: 5,
+        link: "https://es.aliexpress.com/item/1005007575874907.html",
+        image: "https://ae-pic-a1.aliexpress-media.com/kf/E3d239bd019094f9883087d2d6a1b90a3Z/Mi-CARROS-Carro-Carrito-de-Compra-Plegable-Carro-Carrito-Ligero-Bolsa-Termo-90x30x39cm-Resistente-Plegable-con.png_.webp"
+      },
+      {
+        name: "Zapatos",
+        price: "13,89",
+        priority: 3,
+        link: "https://es.aliexpress.com/item/1005005948635620.html",
+        image: "https://ae-pic-a1.aliexpress-media.com/kf/S1bbab1b2700e4dc18bc870793e868350u/Zapatos-Deportivos-informales-para-hombre-zapatillas-peque-as-de-moda-c-modos-Tenis-de-Skate-de.jpg_.webp"
       }
     ]
   };
